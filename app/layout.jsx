0bc319256d4c04b1567a22b492a89228d45a0f16
Veeravel-S <veeravel.igt@gmail.com>
2026-08-19 12:19:03 +0530

    changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick.css";
 import { SITE_LINKS } from "../data/site";
 import InternalLinkHandler from "../components/shared/InternalLinkHandler";
 import MobileCtaBar from "../components/shared/MobileCtaBar";
+import Script from "next/script";
 
 export const metadata = {
   title: "Rio Children's Hospital — Advanced Women & Child Care in Tamil Nadu",
@@ -35,7 +36,32 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5DS2V32X');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5DS2V32X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <InternalLinkHandler />
         {children}
         <MobileCtaBar />
